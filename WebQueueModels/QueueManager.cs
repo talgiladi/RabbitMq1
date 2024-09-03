af7077de4a2d1c4fb87820339e7198b862d44038
talgiladi@gmail.com <talgiladi@gmail.com>
2024-09-03 22:36:00 +0300

    working with timeout

---
+++ WebQueueModels/QueueManager.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebQueueModels
+{
+    public class QueueManager : IDisposable
+    {
+        private IConnection? connection;
+        private IModel? channel;
+        public IModel CreateDeadMessagesQueue()
+        {
+            var rabbitMQUrl = WebQueueModels.Settings.QueueUri;
+
+            var factory = new ConnectionFactory { HostName = rabbitMQUrl };
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+            channel.ExchangeDeclare("dlx_exchange", ExchangeType.Direct);
+            channel.QueueDeclare("dlx_queue", true, false, false, null);
+            channel.QueueBind("dlx_queue", "dlx_exchange", "dlx_routing_key");
+
+            // Declare the original queue with DLX settings
+            var arguments = new Dictionary<string, object>
+            {
+                { "x-dead-letter-exchange", "dlx_exchange" },
+                { "x-dead-letter-routing-key", "dlx_routing_key" },
+                { "x-message-ttl", 10000 } // TTL in milliseconds
+            };
+            channel.QueueDeclare(queue: WebQueueModels.Settings.WorkingQueueName,
+                                 durable: true,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: arguments);
+
+            return channel;
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                this.connection?.Dispose();
+            }
+            catch { }
+            try
+            {
+                this.channel?.Dispose();
+            }
+            catch { }
+        }
+    }
+}
---
+++ WebQueueModels/Settings.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace WebQueueModels
+{
+    public static class Settings
+    {
+        private const string _workingQueueName = "webhook_queue";
+        private static readonly IConfigurationRoot configuration;
+        static Settings()
+        {
+            configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+            Console.WriteLine(configuration == null ? "null" : configuration.ToString());
+            if (configuration == null)
+            {
+                throw new Exception($"configuration section not found");
+            }
+        }
+
+        public static string WorkingQueueName
+        {
+            get { return _workingQueueName; }
+        }
+
+        public static string QueueUri
+        {
+            get
+            {
+                var uri = configuration["RabbitMQUrl"];
+                Console.WriteLine($"RabbitMQUrl: {uri}");
+                if (uri == null)
+                {
+                    throw new Exception($"please set the 'RabbitMQUrl' value in config");
+                }
+
+                return uri;
+            }
+        }
+    }
+}
