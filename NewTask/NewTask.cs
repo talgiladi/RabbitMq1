cb874c11cbee2de2e1fde869d6c11ee5b2615775
talgiladi@gmail.com <talgiladi@gmail.com>
2024-09-03 13:51:43 +0300

    first commit

---
+++ NewTask/NewTask.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Configuration;
+
+internal class Program
+{
+    private static int index;
+    private static void Main(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+        Console.WriteLine(configuration ==null? "null":configuration.ToString());
+        var rabbitMQUrl = configuration["RabbitMQUrl"];
+        Console.WriteLine(rabbitMQUrl);
+        var factory = new ConnectionFactory { HostName = rabbitMQUrl };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(queue: "hello",
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+        int index = 0;
+        while (true)
+        {
+            string message = $"#{index++} {DateTime.UtcNow}";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: string.Empty,
+                                routingKey: "hello",
+                                basicProperties: null,
+                                body: body);
+            Console.WriteLine($" [x] Sent {message}");
+            Thread.Sleep(4000);
+        }
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
---
+++ Worker/Worker.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Configuration;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var host = new HostBuilder()
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddHostedService<QueueConsumerService>();
+            })
+            .Build();
+
+        await host.RunAsync();
+    }
+}
+
+public class QueueConsumerService : BackgroundService
+{
+    private readonly IModel _channel;
+
+    public QueueConsumerService()
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        var rabbitMQUrl = configuration["RabbitMQUrl"];
+        var factory = new ConnectionFactory { HostName = rabbitMQUrl };
+        var connection = factory.CreateConnection();
+        _channel = connection.CreateModel();
+        _channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+        Console.WriteLine(" [*] Waiting for messages.");
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var consumer = new EventingBasicConsumer(_channel);
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            var id = Guid.NewGuid().ToString();
+            Console.WriteLine($" [x] Received {message}. {id}");
+            int dots = message.Split('.').Length - 1;
+            Thread.Sleep(dots * 1000);
+            Console.WriteLine($" [x] Done. {id}");
+        };
+        _channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+        return Task.CompletedTask;
+    }
+
+    public override void Dispose()
+    {
+        _channel.Close();
+        base.Dispose();
+    }
+}
+
