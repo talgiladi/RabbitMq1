8a7601b0b7c074f4d0e9734c9fc69655274ec407
talgiladi@gmail.com <talgiladi@gmail.com>
2024-09-11 16:10:55 +0300

    large files

--- NewTask/NewTask.cs
+++ NewTask/NewTask.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using RabbitMQ.Client;
+using System;
+using System.Diagnostics;
 using System.Text;
 namespace Queues
 {
@@ -10,11 +12,24 @@ namespace Queues
         {
             queueManager = new WebQueueModels.QueueManager();
             var channel = queueManager.CreateMainQueue();
-           
+
             int index = 0;
-            while (true)
+            //while (true)
+            //{
+            //    string message = GetMessage($"#{index++} {Guid.NewGuid()}");
+            //    var body = Encoding.UTF8.GetBytes(message);
+            //    var properties = channel.CreateBasicProperties();
+            //    properties.Persistent = true;
+            //    channel.BasicPublish(exchange: string.Empty,
+            //                        routingKey: WebQueueModels.Settings.WorkingQueueName,
+            //                        basicProperties: properties,
+            //                        body: body);
+            //    Console.WriteLine($" [x] Sent {message}");
+            //}
+            var watch = Stopwatch.StartNew();
+            for (int i = 0; i < 1000; i++)
             {
-                string message = GetMessage($"#{index++} {Guid.NewGuid()}");
+                string message = new('a', 1_800_000);
                 var body = Encoding.UTF8.GetBytes(message);
                 var properties = channel.CreateBasicProperties();
                 properties.Persistent = true;
@@ -22,8 +37,10 @@ namespace Queues
                                     routingKey: WebQueueModels.Settings.WorkingQueueName,
                                     basicProperties: properties,
                                     body: body);
-                Console.WriteLine($" [x] Sent {message}");
+                Console.WriteLine($" [x] Sent #{i}");
             }
+            watch.Stop();
+            Console.WriteLine($"elapsed {watch.Elapsed.TotalSeconds}");
 
         }
 
